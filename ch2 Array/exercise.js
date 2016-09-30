d20204578255fa1ea290f34bd528e28e907465c8
woohling <woohling@126.com>
2016-09-30 17:59:20 +0800

    update code

--- ch2 Array/exercise.js
+++ ch2 Array/exercise.js
@@ -1,3 +1,31 @@
 /**
  * Created by wuhuiling on 2016/9/30.
  */
+
+//1
+function Grades() {
+    this.grades = [];
+    this.average = getAverage;
+    this.add = add;
+}
+
+function getAverage() {
+    var total = 0;
+    for (var i = 0; i < this.grades.length; i++) {
+        total += this.grades[i];
+    }
+    var average = total / this.grades.length;
+    return average;
+}
+
+function add(grade) {
+    this.grades.push(grade);
+}
+
+var studentA = new Grades();
+studentA.add(98);
+studentA.add(68);
+studentA.add(88);
+
+var average = studentA.average().toFixed(2);
+console.log(average);
