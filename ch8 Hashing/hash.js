3aa7a09c664a657344f896f64796c2a4d95e1033
woohling <woohling@126.com>
2016-10-21 08:08:08 +0800

    ch8

---
+++ ch8 Hashing/hash.js
@@ -0,0 +1,28 @@
+function Hash() {
+    this.table = new Array(137);
+    this.simpleHash = simpleHash;
+    this.put = put;
+    this.showDistro = showDistro;
+}
+
+function simpleHash(data) {
+    var total = 0;
+    for (var i = 0; i < data.length; ++i) {
+        total += data.charCodeAt(i);
+    }
+    return total % this.table.length;
+}
+
+function put(data) {
+    var pos = this.simpleHash(data);
+    this.table[pos] = data;
+}
+
+function showDistro() {
+    var n = 0;
+    for (var i = 0; i < this.table.length; i++) {
+        if (this.table[i] !== undefined) {
+            console.log(this.table[i]);
+        }
+    }
+}
