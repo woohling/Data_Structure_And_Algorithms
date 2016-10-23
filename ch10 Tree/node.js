33ffdaba1b3aba5bc1a314d9e47ec6113265f062
woohling <woohling@126.com>
2016-10-23 15:13:26 +0800

    update code

--- ch10 Tree/node.js
+++ ch10 Tree/node.js
@@ -18,6 +18,7 @@ function BST() {
     this.getMin = getMin;
     this.getMax = getMax;
     this.find = find;
+    this.remove = remove;
 }
 
 function insert(data) {
@@ -97,6 +98,51 @@ function find(data) {
         if (current == null) {
             return null;
         }
+    }
+    return current;
+}
+
+function remove(data) {
+    this.root = removeNode(this.root, data);
+}
+
+function removeNode(node, data) {
+    if (node == null) {
+        return null;
+    }
+    if (data = node.data) {
+        // node has no children
+        if (node.left == null && node.right == null) {
+            return null;
+        }
+        // node has no left child
+        if (node.left == null) {
+            return node.right;
+        }
+        // node has no right child
+        if (node.right == null) {
+            return node.left;
+        }
+        //node has two children
+        var tempNode = getSmallest(node.right);
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+    }
+    else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+    }
+    else {
+        node.right = removeNode(node.right, data);
+        return node;
+    }
+}
+
+function getSmallest(node) {
+    var current = node;
+    while (current.left !== null) {
+        current = current.left;
     }
     return current;
 }
